e197e24b8e5b09843db362a5ec5ccdd6481998a4
alalUDDIN123 <alalu75666@gmail.com>
2023-06-01 19:58:58 +0530

    added_cors_library

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const PORT = process.env.PORT || 8080
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const PORT = process.env.PORT || 8080;
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
 
-server.use(middlewares)
-server.use(router)
+// Enable CORS
+const cors = require('cors');
+server.use(cors());
+
+server.use(middlewares);
+server.use(router);
 server.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`)
-})
+  console.log(`Server running on http://localhost:${PORT}`);
+});
